5ccd813cd90ee59fa1c62dd7e9943b4ecf9813cb
nicwu25 <nicwu25@gmail.com>
2023-05-11 12:59:06 +0800

    Initial Commit

---
+++ 104Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  104Test
+//
+//  Created by Nic Wu on 2023/5/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
